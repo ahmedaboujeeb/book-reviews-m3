e756e93712955ccb1dcd509ae37e1d9623e9e4d9
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-01-11 15:44:34 +0100

    Add add review form

--- app.py
+++ app.py
@@ -90,6 +90,11 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
+
+@app.route("/add_review", methods=["GET", "POST"])
+def add_review():
+    return render_template("add_review.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
